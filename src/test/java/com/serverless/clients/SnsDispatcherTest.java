c72cc6d30a34213a8fde5155cd4a8ead92d7aa5d
Jonathan Yu <oninuva@gmail.com>
2019-03-01 01:08:36 +0000

    Add maven surefire plugin for running unit tests
    * Moved tests from /tst/com/serverless to /src/test/java/com/serverless

--- src/test/java/com/serverless/clients/SnsDispatcherTest.java
+++ src/test/java/com/serverless/clients/SnsDispatcherTest.java
@@ -10,7 +10,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class SnsDispatcherTest {
+public class SnsDispatcherTest {
 
     private static final String TOPIC_ARN = "TOPIC_ARN";
     private static final String TEST_MESSAGE = "Test Message";
@@ -19,14 +19,14 @@ class SnsDispatcherTest {
     private AmazonSNS mockClient;
 
     @Test
-    void dispatch() {
+    public void dispatch() {
         SnsDispatcher dispatcher = new SnsDispatcher(mockClient, TOPIC_ARN);
         dispatcher.dispatch(TEST_MESSAGE);
         Mockito.verify(mockClient).publish(TOPIC_ARN, TEST_MESSAGE);
     }
 
     @Test
-    void dispatchException() {
+    public void dispatchException() {
         Mockito.when(mockClient.publish(TOPIC_ARN, TEST_MESSAGE))
                 .thenThrow(new AmazonClientException("Exception"));
         SnsDispatcher dispatcher = new SnsDispatcher(mockClient, TOPIC_ARN);
